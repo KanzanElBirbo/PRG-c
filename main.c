3ee9892d10a6a78b7e2702eeed7b4c43d0b30509
KanzanElBirbo <76937223+KanzanElBirbo@users.noreply.github.com>
2021-01-05 22:05:50 +0100

    Násobilka

--- main.c
+++ main.c
@@ -3,5 +3,19 @@
 #include <math.h>
 
 int main() {
-printf("Ahoj Svete\n");
+    int x;
+    int y;
+    int z;
+    printf("Nasobilka:\n");
+    for(x=1; x<=10; x++){
+        for(y=0; y<=20; y++){
+            if(y==0){
+                printf("%4i/",x);
+            }else{
+                z = x * y;
+                printf("%4i/", z);
+            }
+        }
+        printf("\n");
+    }
 }
