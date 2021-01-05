452769a507c0fbc73847b046c1dcdadfae010ae3
KanzanElBirbo <76937223+KanzanElBirbo@users.noreply.github.com>
2021-01-05 22:07:21 +0100

    Ahoj Světe

---
+++ as.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+int x;
+int y;
+
+
+scanf("%d %d", &x, &y);
+if(x > 0) {
+    x *= -1;
+}
+return x;
+}
+
+sum = x + y; {
+printf("%i", &sum);
+}
